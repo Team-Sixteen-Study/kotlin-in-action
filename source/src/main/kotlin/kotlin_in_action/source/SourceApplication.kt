14f232bd90f810e20bedb18524738c4df70085b4
Goryeojin <sooon9254@naver.com>
2024-12-21 17:09:42 +0900

    init: kotlin-in-action first commit

---
+++ source/src/main/kotlin/kotlin_in_action/source/SourceApplication.kt
@@ -0,0 +1,11 @@
+package kotlin_in_action.source
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SourceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SourceApplication>(*args)
+}
