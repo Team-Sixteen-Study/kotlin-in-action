2c76312277c414ac7d4b22887e413c631b688df8
Goryeojin <sooon9254@naver.com>
2025-01-02 00:21:53 +0900

    add: chapter 5-1 소스 코드 작성

---
+++ ryeojin/source/src/main/kotlin/ch05/5.1.4_2_AccessingVariablesInScope2.kt
@@ -0,0 +1,19 @@
+package ch05.AccessingVariablesInScope
+
+fun printProblemCounts(response: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    response.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        } else if (it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+    println("$clientErrors client errors, $serverErrors server errors")
+}
+
+fun main() {
+    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+    printProblemCounts(responses)
+}
